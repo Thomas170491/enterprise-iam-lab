1f023a0d0a59cfc001de96284733144367ca732d
Thomas <thomaspapas470@gmail.com>
2026-08-11 19:27:08 +0200

    Added OAuth config and registration

--- apps/employee-portal/app.py
+++ apps/employee-portal/app.py
@@ -1,20 +1,40 @@
-from flask import Flask 
 
-from config import Config 
+
+
+from flask import Flask 
+from config import Config
 from flask_session import Session
 from portal.routes import bp_portal 
+from auth.__init__ import oauth
+
 
 sess = Session()
 
+
 def create_app():
+
     #Initialize app
     app = Flask(__name__)
 
     #Grab configuration from config.py here
     app.config.from_object(Config)
 
-    #Initialize sessions
+    #Initialize sessionsand OAuth
     sess.init_app(app)
+    oauth.init_app(app)
+
+    #OAuth registration
+    oauth.register(
+        name="keycloak",
+        client_id  = app.config["KEYCLOAK_CLIENT_ID"],
+        client_secret  = app.config["KEYCLOAK_CLIENT_SECRET"],
+        server_metadata_url =  app.config["KEYCLOAK_METADATA_URL"],
+        client_kwargs={
+            "scope" : "openid profile email"
+        }
+    )
+
+
 
     #Register blueprints
     app.register_blueprint(bp_portal)
