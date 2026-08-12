5da83080c2cedc61257d285b1c80041653aa03d0
Thomas <thomaspapas470@gmail.com>
2026-08-12 11:30:49 +0200

    Added user session info and made home.html session specific

--- apps/employee-portal/portal/routes.py
+++ apps/employee-portal/portal/routes.py
@@ -1,7 +1,12 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,session
 
 
 bp_portal = Blueprint("portal", __name__)
 @bp_portal.route("/")
 def home():
-    return render_template("home.html")
+    user = session.get("user")
+
+    return render_template(
+        "home.html",
+        user = user
+        )
