c4e367c7658dea843ab83322d9aa798b7e325af7
Thomas <thomaspapas470@gmail.com>
2026-08-14 12:55:11 +0200

    Added RBAC decorators

---
+++ apps/employee-portal/auth/decorators.py
@@ -0,0 +1,26 @@
+from functools import wraps
+
+from flask import abort
+from flask_login import current_user
+
+def realm_roles_required(required_role):
+    def decorator(view_function):
+        @wraps(view_function)
+        def wrapped_view(*args, **kwargs):
+            if required_role not in current_user.realm_roles :
+                return abort(403)
+
+            return view_function(*args,**kwargs)
+        return wrapped_view
+    return decorator
+
+def client_roles_required(required_role):
+    def decorator(view_function):
+        @wraps(view_function) 
+        def wrapped_view(*args,**kwargs):
+            if required_role not in current_user.client_roles:
+                return abort(403)
+            return view_function(*args,**kwargs)
+        return wrapped_view
+    return decorator
+
